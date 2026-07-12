979df02b009bd723fb7fb417eaea8bfa101ecc4b
Harsh <userharsh911@gmail.com>
2026-07-12 23:05:46 +0530

    can form rect: correct version

--- question/isRect.cpp
+++ question/isRect.cpp
@@ -4,24 +4,16 @@ using namespace std;
 int main()
 {
     int a = 10;
-    int b = 5;
+    int b = 10;
     int c = 10;
     int d = 5;
     int count = 0;
-    if(a==b || a==c || a==d){
-        count++;
-        if(b==c || b==d){
-            count++;
-        }else if(c==d){
-            count++;
-        }
-    }
-
-    if(count==2){
+    if((a==b && c==d) || (b==c && d==a)){
         cout<<"can form rectangle";
     }else{
         cout<<"can not form rectangle";
-    }
+    }  
+    
     
     return 0;    
 }
