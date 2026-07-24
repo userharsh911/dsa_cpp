ec8897a4dfc791d8d9fa48a1947102136aa16cc0
Harsh <userharsh911@gmail.com>
2026-07-24 21:10:32 +0530

    updated files armStrong

--- function/armStrongNumber.cpp
+++ function/armStrongNumber.cpp
@@ -10,20 +10,19 @@ int count_digit(int n){
     return i;
 }
 
+//  binary exponention 
+
 int power(int base, int n){
-    int num = 1;
-    for(int i=1;i<=base/2;i++){
-        num*=n;
+    int ans = 1;
+    while(base>0){
+        if(base%2==1) ans *= n; 
+        n *= n;
+        base /= 2;
     }
-    if(base%2!=0){
-        num = num*num*n;
-    }else{
-        num = num*num;
-    }
-    return num;
+    return ans;
 }
 
-int isArmStrong(int n){
+bool isArmStrong(int n){
     int v = n;
     int ans = 0;
     int digit = count_digit(n);
@@ -32,11 +31,11 @@ int isArmStrong(int n){
         n/=10;
         ans+=power(digit,rem);
     }
-    if(v==ans) return 1;
-    return 0;
+    if(v==ans) return true;
+    return false;
 }
 
 int main(){
-    cout<<isArmStrong(153);
+    cout<<isArmStrong(153)<<endl;
     return 0;
 }
