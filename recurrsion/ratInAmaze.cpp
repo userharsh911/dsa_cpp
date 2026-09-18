bf7ad58f99c1a923d5775e104080a2893da8b780
Harsh <userharsh911@gmail.com>
2026-09-18 15:19:41 +0530

    recursion: problems solved and rat in a maze solved too

--- recurrsion/ratInAmaze.cpp
+++ recurrsion/ratInAmaze.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 using namespace std;
 
-void ratInAMaze(vector<vector<int>> v, vector<vector<vector<int>>> &coordinates, vector<vector<int>> set, int i = 0, int j = 0)
+void ratInAMaze(vector<vector<int>>& v, vector<vector<vector<int>>> &coordinates, vector<vector<int>>& set, int i = 0, int j = 0)
 {
     if (i == v.size() || j == v.size() || i < 0 || j < 0 || v[i][j] == 0)
         return;
     set.push_back({i, j});
     if (i == j && i == v.size() - 1){
         coordinates.push_back(set);
+        set.pop_back();
         return;
     }
 
@@ -26,6 +27,8 @@ void ratInAMaze(vector<vector<int>> v, vector<vector<vector<int>>> &coordinates,
         ratInAMaze(v, coordinates, set, dir[0], dir[1]);
         skip:;
     }
+
+    set.pop_back();
 }
 
 int main()
