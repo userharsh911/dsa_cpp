1db582c18772c10c6e6a502bf8714e605f9544c7
Harsh <userharsh911@gmail.com>
2026-09-18 16:43:10 +0530

    recursion: josephus problem

--- recurrsion/josephusProblem.cpp
+++ recurrsion/josephusProblem.cpp
@@ -2,30 +2,39 @@
 #include <vector>
 using namespace std;
 
+
+int winner(int n, int k){
+    if(n == 0) return n;
+    return (winner(n - 1, k) + k) % n;
+}
+
+
 int main(){
     int n = 6;
     int k = 5;
-    k = k % n;
-    int person_left = n - 1;
-    vector<bool> persons(n, 0);
-    int index = 0;
-    int count = k;
+    cout<<winner(n, k) + 1;
     
-    while(person_left){
-        index = index % n;
-        if(!persons[index]) count--;
+    // k = k % n;
+    // int person_left = n - 1;
+    // vector<bool> persons(n, 0);
+    // int index = 0;
+    // int count = k;
+    
+    // while(person_left){
+    //     index = index % n;
+    //     if(!persons[index]) count--;
 
-        if(!count){
-            persons[index] = true;
-            count = k;
-            person_left--;
-            cout<<index + 1<<" ";
-        }
-        index++;
-    }
-    cout<<endl;
-    for(int i = 0; i < persons.size(); i++)
-        if(!persons[i]) cout<<i + 1<<" ";
+    //     if(!count){
+    //         persons[index] = true;
+    //         count = k;
+    //         person_left--;
+    //         cout<<index + 1<<" ";
+    //     }
+    //     index++;
+    // }
+    // cout<<endl;
+    // for(int i = 0; i < persons.size(); i++)
+    //     if(!persons[i]) cout<<i + 1<<" ";
    
 
     return 0;
